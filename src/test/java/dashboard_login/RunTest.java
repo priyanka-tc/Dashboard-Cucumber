6835457b58267505e4e28ce5761923bbbbd5c0d2
Priyanka Dhampalwar <priyankadhampalwar@gmail.com>
2014-08-05 15:13:56 +0100

    feature file for log in

---
+++ src/test/java/dashboard_login/RunTest.java
@@ -0,0 +1,12 @@
+package dashboard_login;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(features="dashboard_login/login.feature")
+public class RunTest {
+
+}
