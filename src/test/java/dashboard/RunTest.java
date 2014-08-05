5e425a6e30a2ff02ff596e914380d82de015c54d
Naresh Kumar <nareshkmr38@gmail.com>
2014-08-05 13:54:25 +0100

    Added TODO list and written feature file for dashboard main page

--- src/test/java/dashboard/RunTest.java
+++ src/test/java/dashboard/RunTest.java
@@ -1,5 +1,12 @@
 package dashboard;
 
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options(features="dashboard/dashboard.feature")
+
 public class RunTest {
 
 }
